d7b3f5ebc25804299d78fbfc40a755c1f4608dd9
Kristian Dunne <KristianDunne@icloud.com>
2021-01-16 15:34:21 +0000

    Configure tailwind custom colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   purge: ['./pages/**/*.js', './components/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        violet: '#5964E0',
+        'light-violet': '#939BF4',
+        'very-dark-blue': '#19202D',
+        midnight: '#121721',
+        'light-grey': '#F4F6F8',
+        gray: '#9DAEC2',
+        'dark-grey': '#6E8098',
+      },
+    },
   },
   variants: {
     extend: {},
