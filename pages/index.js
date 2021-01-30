cad76b3e357d431d576e0e54f793b5a853f64428
Kristian Dunne <KristianDunne@icloud.com>
2021-01-30 16:00:50 +0000

    Change the number of jobs per row depending on screen size

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="flex flex-col items-center mb-16">
-        <div className="px-6 top-24 ">{pages}</div>
+        <div className="mx-6 top-24 md:grid-cols-2 md:gap-2.5 md:grid md:px-10 xl:grid-cols-3 xl:gap-8 lg:px-40">
+          {pages}
+        </div>
         <button
           className="w-32 py-4 mt-8 font-bold leading-4 text-white rounded-md bg-violet"
           onClick={() => setPageIndex(pageIndex + 1)}>
